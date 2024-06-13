d1c38af397aa28118de2a92f9ba2303538ef12bf
Ricesc <ricesc14@gmail.com>
2024-06-13 14:17:26 -0400

    first commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import ListaTareas from "../ListaTareas/ListaTareas";
+
+function App() {
+    return (
+        <div>
+            <h1>Hello Dojo!</h1>
+            <h2>Things I need to do:</h2>
+            <ListaTareas />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/ListaTareas/ListaTareas.jsx
@@ -0,0 +1,12 @@
+const ListaTareas = () => {
+    return(
+        <ul>
+            <li>Learn React</li>
+            <li>Climb Mt. Everest</li>
+            <li>Run a marathon</li>
+            <li>Feed the dogs</li>
+        </ul>
+    );
+}
+
+export default ListaTareas;
