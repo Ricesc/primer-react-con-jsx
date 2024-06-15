757958ac31d625fa7cd9b7a62beb1e8c1ca89818
Ricesc <ricesc14@gmail.com>
2024-06-15 03:10:50 -0400

    minor changes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,11 +2,11 @@ import ListaTareas from "../ListaTareas/ListaTareas";
 
 function App() {
     return (
-        <div>
+        <>
             <h1>Hello Dojo!</h1>
             <h2>Things I need to do:</h2>
             <ListaTareas />
-        </div>
+        </>
     );
 }
 
--- src/components/ListaTareas/ListaTareas.jsx
+++ src/components/ListaTareas/ListaTareas.jsx
@@ -1,11 +1,13 @@
 const ListaTareas = () => {
     return(
-        <ul>
-            <li>Learn React</li>
-            <li>Climb Mt. Everest</li>
-            <li>Run a marathon</li>
-            <li>Feed the dogs</li>
-        </ul>
+        <>
+            <ul>
+                <li>Learn React</li>
+                <li>Climb Mt. Everest</li>
+                <li>Run a marathon</li>
+                <li>Feed the dogs</li>
+            </ul>
+        </>
     );
 }
 
